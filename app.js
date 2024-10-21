196418fea67611886cba800426c0a02c90e76ea0
brutalKim <132333588+brutalKim@users.noreply.github.com>
2024-10-22 01:53:17 +0900

    addclient

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const http = require("http");
-
+const path = require('path');
 const AuthFilter = require('./MiddleWare/AuthFilter');
 const AuthRouter = require('./Controller/AuthRouter');
 const PrivateRouter = require('./Controller/PrivateRouter');
@@ -17,7 +17,10 @@ RuletServer(server);
 app.use(express.json());
 app.use('/api',AuthFilter,PrivateRouter)
 app.use('/authapi',AuthRouter);
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.listen(8484, () => {
     console.log(`Example app listening`)
+})
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/build/index.html'));
 })
